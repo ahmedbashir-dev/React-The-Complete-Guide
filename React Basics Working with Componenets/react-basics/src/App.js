4215991b433f0d57454a4a2cfa2ca7e377c0b276
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-01-15 01:39:54 +0500

    Added Expense form with states

--- React Basics Working with Componenets/react-basics/src/App.js
+++ React Basics Working with Componenets/react-basics/src/App.js
@@ -1,6 +1,6 @@
 
 import Expenses from "./components/Expenses/Expenses";
-
+import NewExpense from "./components/NewExpense/NewExpense";
 const App = ()=> {
 	const expenses = [
 		{
@@ -30,8 +30,8 @@ const App = ()=> {
 	];
 	return (
 		<div>
-			<h2>Let's get started</h2>
-      <Expenses items={expenses}/>
+			<NewExpense/>
+     		 <Expenses items={expenses}/>
 		</div>
 	);
 }
---
+++ React Basics Working with Componenets/react-basics/src/components/NewExpense/NewExpense.js
@@ -0,0 +1,11 @@
+import ExpenseForm from './ExpenseForm';
+import './NewExpense.css';
+const NewExpense = (props)=>{
+    return(
+        <div className='new-expense'>
+            <ExpenseForm/>
+        </div>
+    );
+}
+
+export default NewExpense;
