a526f82e4cfc6b47b459bfa3a3b2b1de6294c7ee
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-01-14 22:27:42 +0500

    Click Handler added for ExpenseItem

--- React Basics Working with Componenets/react-basics/src/components/Expenses/ExpenseItem.js
+++ React Basics Working with Componenets/react-basics/src/components/Expenses/ExpenseItem.js
@@ -3,7 +3,9 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 const ExpenseItem = (props) => {
-	
+	const clickHandler = ()=>{
+		console.log("Clicked!!");
+	}
 	return (
 		<Card className="expense-item">
 			<ExpenseDate date = {props.date}/>
@@ -11,7 +13,7 @@ const ExpenseItem = (props) => {
 				<h2>{props.title}</h2>
 				<div className='expense-item__price'>${props.amount}</div>
 			</div>
-			<button>Change Title</button>
+			<button onClick={clickHandler}>Change Title</button>
 		</Card>
 	);
 }
