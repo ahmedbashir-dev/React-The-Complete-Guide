4eea71430d162dce5a0918cc46f8160906257e07
Ahmed Bashir <ahmedbashir.w@gmail.com>
2022-01-15 02:10:52 +0500

    Form submission code added

--- React Basics Working with Componenets/react-basics/src/components/NewExpense/ExpenseForm.js
+++ React Basics Working with Componenets/react-basics/src/components/NewExpense/ExpenseForm.js
@@ -2,57 +2,77 @@ import { useState } from 'react';
 import './ExpenseForm.css';
 
 const ExpenseForm = ()=>{
-    // const [enteredTitle, setEnteredTitle] = useState('');
-    // const [enteredAmount, setEnteredAmount] = useState('');
-    // const [selectedDate, setSelectedDate] = useState('');
+    const [enteredTitle, setEnteredTitle] = useState('');
+    const [enteredAmount, setEnteredAmount] = useState('');
+    const [selectedDate, setSelectedDate] = useState('');
 
     //Using single state 
-    const [userInput, setUserInput] = useState({
-        enteredTitle:'',
-        enteredAmount:'',
-        selectedDate:''
-    })
+    // const [userInput, setUserInput] = useState({
+    //     enteredTitle:'',
+    //     enteredAmount:'',
+    //     selectedDate:''
+    // })
     const titleChangeHandler = (e)=>{
-        //setEnteredTitle(e.target.value);
-        setUserInput({
-            ...userInput,
-            enteredTitle:e.target.value
-        })
+        setEnteredTitle(e.target.value);
+        
+        //This is not a proper way to update multiple states
+        // setUserInput({
+        //     ...userInput,
+        //     enteredTitle:e.target.value
+        // })
+
+        //This one is a proper way
+        // setUserInput((prevState)=>{
+        //     return {...prevState, enteredAmount:e.target.value}
+        // });
     }
     const amountChangeHandler = (e)=>{
-        // setEnteredAmount(e.target.value);
-        setUserInput({
-            ...userInput,
-            enteredAmount:e.target.value
-        })
+        setEnteredAmount(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     enteredAmount:e.target.value
+        // })
     }
     const dateChangeHandler = (e)=>{
-        // setSelectedDate(e.target.value);
-        setUserInput({
-            ...userInput,
-            selectedDate:e.target.value
-        })
+        setSelectedDate(e.target.value);
+        // setUserInput({
+        //     ...userInput,
+        //     selectedDate:e.target.value
+        // })
+    }
+
+    const submitHandler = (e)=>{
+        e.preventDefault();
+        const expenseData = {
+            title:enteredTitle,
+            amount:enteredAmount,
+            date: new Date(selectedDate)
+        }
+        setEnteredAmount('');
+        setEnteredTitle('');
+        setSelectedDate('');
+        console.log(expenseData);
     }
     return(
-        <form>
+        <form onSubmit={submitHandler}>
             <div className='new-expense__controls'>
                 <div className='new-expense__control'>
                     <label>Title</label>
-                    <input type="text" onChange={titleChangeHandler} />
+                    <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Amount</label>
-                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                    <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
 
                 <div className='new-expense__control'>
                     <label>Date</label>
-                    <input type="date" min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
+                    <input type="date" value={selectedDate} min="2019-01-01" max="2022-12-31" onChange={dateChangeHandler} />
                 </div>
             </div>
             <div className='new-expense__actions'>
-                <button type='submit'>Add Expense</button>
+                <button type='submit' >Add Expense</button>
             </div>
         </form>
     )
